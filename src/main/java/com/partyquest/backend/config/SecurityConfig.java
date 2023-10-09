496e57c5f620afd2b1b209a1cf92b8e72ba2507a
SongMinGyu <ggp04114@naver.com>
2023-10-10 01:43:35 +0900

    chore(redis): add formLogin::disable
    formLogin::disable:
    - Do not use formLogin, which is used by default in SpringSecurity.
    
    BREAKING CHANGE: SecurityConfig.filterChain

--- src/main/java/com/partyquest/backend/config/SecurityConfig.java
+++ src/main/java/com/partyquest/backend/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,7 +19,8 @@ public class SecurityConfig {
         http
                 .httpBasic(HttpBasicConfigurer::disable)
                 .csrf(CsrfConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable);
+                .cors(AbstractHttpConfigurer::disable)
+                .formLogin(FormLoginConfigurer::disable);
         return http.build();
     }
 
