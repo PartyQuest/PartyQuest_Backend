90edc71891ac95a792546c07d006b9f44ad33224
SongMinGyu <ggp04114@naver.com>
2023-10-10 03:41:25 +0900

    github action test 1

--- src/test/java/com/partyquest/backend/controller/TestControllerTest.java
+++ src/test/java/com/partyquest/backend/controller/TestControllerTest.java
@@ -1,6 +1,5 @@
 package com.partyquest.backend.controller;
 
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +8,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -26,10 +26,8 @@ public class TestControllerTest {
     @DisplayName("test01")
     void test01() throws Exception {
         mockMvc.perform(
-                get("/test01/t1")).andDo(
-                        document("/t1",
-                                preprocessRequest(prettyPrint()),
-                                preprocessResponse(prettyPrint()))
+                get("/test01/t1"))
+                .andDo(document("/t1", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()))
         ).andExpect(status().isOk());
     }
 }
