6804056b5fe0a1bf93e6b17399fcce3d1bbd627d
SongMinGyu <ggp04114@naver.com>
2023-10-10 02:22:21 +0900

    github action test 1

--- src/main/java/com/partyquest/backend/controller/TestController.java
+++ src/main/java/com/partyquest/backend/controller/TestController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/*
+* TEST ONLY !!!
+* */
 @RestController
 @RequestMapping("/test01")
 public class TestController {
