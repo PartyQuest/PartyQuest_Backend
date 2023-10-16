56103947ea68ef4ff6ee2d6146b9be43c0573955
SongMinGyu <ggp04114@naver.com>
2023-10-16 16:25:10 +0900

    chore(Auth): add login/out test
    modify dto data and Error source

--- src/test/java/com/partyquest/backend/service/impl/AuthServiceImplTest.java
+++ src/test/java/com/partyquest/backend/service/impl/AuthServiceImplTest.java
@@ -2,7 +2,9 @@ package com.partyquest.backend.service.impl;
 
 import com.partyquest.backend.domain.dto.AuthDto;
 import com.partyquest.backend.domain.entity.User;
+import com.partyquest.backend.domain.repository.UserRepository;
 import com.partyquest.backend.service.logic.AuthService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,10 +16,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AuthServiceImplTest {
 
     private final AuthServiceImpl authService;
+    private final UserRepository userRepository;
 
     @Autowired
-    AuthServiceImplTest(AuthServiceImpl authService) {
+    AuthServiceImplTest(AuthServiceImpl authService, UserRepository userRepository) {
         this.authService = authService;
+        this.userRepository = userRepository;
     }
 
 
@@ -31,17 +35,29 @@ class AuthServiceImplTest {
                 .build();
         AuthDto.SignupResponseDto signup = authService.Signup(dto, "LOCAL");
         assertAll(
-                () -> assertEquals(signup.getId(), 1),
                 () -> assertEquals(signup.getEmail(), "email")
         );
+    }
+    @AfterEach
+    void clear() {
+        userRepository.deleteAll();
     }
 
     @Test
     void LoginTest() {
-        SignUpTest();
+
+        AuthDto.SignupDto dtos = AuthDto.SignupDto.builder()
+                .email("emails")
+                .birth("birth")
+                .nickname("nickname")
+                .password("password")
+                .build();
+        AuthDto.SignupResponseDto signup = authService.Signup(dtos, "LOCAL");
+
+
         AuthDto.LoginRequestDto dto = AuthDto.LoginRequestDto.builder()
-                .email("email2")
-                .password("password2")
+                .email("emails")
+                .password("password")
                 .build();
         AuthDto.LoginResponseDto login = authService.Login(dto);
         System.out.println(login.toString());
@@ -49,7 +65,6 @@ class AuthServiceImplTest {
 
     @Test
     void LogoutTest() {
-        LoginTest();
         authService.Logout(0);
     }
 }
